0ebf0139f67f5e82466f36700e77ef8cc3e9391d
anjeylink <andriy.oprysko@resultify.se>
2018-12-18 14:28:11 +0200

    [TASK] use array brackets in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,41 +10,37 @@
  * writing. "version" and "dependencies" must not be touched!
  ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array(
-	'title' => 'RealURL 404 multilingual error page',
-	'description' => 'Shows the defined error page of the given language if the requested page or file could not be found',
-	'category' => 'fe',
-	'shy' => 0,
-	'version' => '1.0.10',
-	'conflicts' => 'error_404_handling',
-	'priority' => '',
-	'loadOrder' => '',
-	'module' => '',
-	'state' => 'stable',
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'modify_tables' => '',
-	'clearcacheonload' => 0,
-	'lockType' => '',
-	'author' => 'Sven Wappler',
-	'author_email' => 'typo3YYYY@wappler.systems',
-	'author_company' => 'WapplerSystems',
-	'CGLcompliance' => '',
-	'CGLcompliance_note' => '',
-	'constraints' => array(
-		'depends' => array(
-			'realurl' => '',
-			'typo3' => '6.0.0-8.7.99',
-		),
-		'conflicts' => array(
-			'error_404_handling' => '',
-		),
-		'suggests' => array(
-		),
-	),
-	'suggests' => array(
-	),
-	'_md5_values_when_last_written' => 'a:4:{s:12:"ext_icon.gif";s:4:"789d";s:17:"ext_localconf.php";s:4:"4d38";s:30:"Classes/Hooks/FrontendHook.php";s:4:"ac32";s:14:"doc/manual.sxw";s:4:"839f";}',
-);
-
-?>
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'RealURL 404 multilingual error page',
+    'description' => 'Shows the defined error page of the given language if the requested page or file could not be found',
+    'category' => 'fe',
+    'shy' => 0,
+    'version' => '1.0.10',
+    'conflicts' => 'error_404_handling',
+    'priority' => '',
+    'loadOrder' => '',
+    'module' => '',
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearcacheonload' => 0,
+    'lockType' => '',
+    'author' => 'Sven Wappler',
+    'author_email' => 'typo3YYYY@wappler.systems',
+    'author_company' => 'WapplerSystems',
+    'CGLcompliance' => '',
+    'CGLcompliance_note' => '',
+    'constraints' => [
+        'depends' => [
+            'realurl' => '',
+            'typo3' => '8.7.0-9.5.99',
+        ],
+        'conflicts' => [
+            'error_404_handling' => '',
+        ],
+        'suggests' => [],
+    ],
+    'suggests' => [],
+    '_md5_values_when_last_written' => 'a:4:{s:12:"ext_icon.gif";s:4:"789d";s:17:"ext_localconf.php";s:4:"4d38";s:30:"Classes/Hooks/FrontendHook.php";s:4:"ac32";s:14:"doc/manual.sxw";s:4:"839f";}',
+];
